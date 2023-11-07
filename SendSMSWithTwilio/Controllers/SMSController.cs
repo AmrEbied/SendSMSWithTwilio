1ec3685161b522bc99bee63eb589be1a8fe1bca6
Amr Ebied <amrebied2040@gmail.com>
2023-11-07 11:28:25 +0200

    Add project files.

---
+++ SendSMSWithTwilio/Controllers/SMSController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SendSMSWithTwilio.Dto;
+using SendSMSWithTwilio.Services;
+
+namespace SendSMSWithTwilio.Controllers
+{
+    
+        [Route("api/[controller]")]
+        [ApiController]
+        public class SMSController : ControllerBase
+        {
+            private readonly ISMSService _smsService;
+
+            public SMSController(ISMSService smsService)
+            {
+                _smsService = smsService;
+            }
+
+            [HttpPost("send")]
+            public IActionResult Send(SendSMSDto dto)
+            {
+                var result = _smsService.Send(dto.MobileNumber, dto.Body);
+
+                if (!string.IsNullOrEmpty(result.ErrorMessage))
+                    return BadRequest(result.ErrorMessage);
+
+                return Ok(result);
+            }
+        }
+    
+}
